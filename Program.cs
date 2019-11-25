6d85a6d0957899418dc24b6812a772b0c283c1ce
sbkat <kathydangg@gmail.com>
2019-11-25 08:45:52 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an empty List of type object:
+            var list = new List<object>();
+
+            // Add the following values to the list: 7, 28, -1, true, "chair":
+            list.Add(7);
+            list.Add(28);
+            list.Add(-1);
+            list.Add(true);
+            list.Add("chair");
+
+            // Loop through the list and print all values:
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Add all values that are Int type together and output the sum:
+            int sum = 0;
+            foreach (var item in list)
+            {
+                if(list is int) {
+                    sum += (int)list;
+                    Console.WriteLine(sum);
+                }
+            }
+        }
+    }
+}
